844993cde1c4eff0600eef678a3e4c7646e29050
Wilman Ortiz <willman.ortiz@gmail.com>
2021-04-22 20:55:20 -0500

    feat: Crea la base del proyecto y todos los archivos de configuracion

---
+++ src/test/java/co/com/demo/bills/SvcBillsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.demo.bills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvcBillsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
