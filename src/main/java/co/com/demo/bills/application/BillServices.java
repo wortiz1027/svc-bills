efff6b4a4c5d4e6e8f2ecbe3c00f9bb39e70d6f1
Wilman Ortiz <willman.ortiz@gmail.com>
2021-04-23 20:52:19 -0500

    fix: Ajusta manejo del detalle de la factura

--- src/main/java/co/com/demo/bills/application/BillServices.java
+++ src/main/java/co/com/demo/bills/application/BillServices.java
@@ -4,8 +4,11 @@ import co.com.demo.bills.domain.controller.Client;
 import co.com.demo.bills.domain.controller.Product;
 import co.com.demo.bills.domain.database.Bill;
 import co.com.demo.bills.domain.database.Detail;
+import co.com.demo.bills.exceptions.bussines.BillNotFoundException;
 import co.com.demo.bills.infraestructure.repository.BillRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -15,16 +18,18 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class BillServices implements BillBehavior {
-
+    private static final Logger LOG = LoggerFactory.getLogger(BillServices.class);
     private final BillRepository repository;
 
     @Override
     public Optional<co.com.demo.bills.domain.controller.Bill> getBillById(String sequence) {
-
         Optional<Bill> bill = this.repository.findById(sequence);
-        // TODO lanzar exception
+
+        if (!bill.isPresent()) throw new BillNotFoundException(String.format("There is no information for bill with sequence: [%s]", sequence));
+
         Optional<List<Detail>> details = this.repository.findAllDetail(bill.get().getId());
-        // TODO lanzar exception
+
+        if (details.isEmpty()) throw new BillNotFoundException(String.format("There is no products information for bill with sequence: [%s]", sequence));
 
         // TODO hacer llamado queue informacion cliente
 
@@ -64,6 +69,48 @@ public class BillServices implements BillBehavior {
 
     @Override
     public Optional<List<co.com.demo.bills.domain.controller.Bill>> getAllBills() {
-        return Optional.empty();
+        Optional<List<Bill>> bills = this.repository.findAlls();
+
+        if (bills.isEmpty()) throw new BillNotFoundException("There is no bills informations");
+
+        List<co.com.demo.bills.domain.controller.Bill> billList = new ArrayList<>();
+
+        for (Bill row : bills.get()) {
+            List<co.com.demo.bills.domain.controller.Detail> detailList = new ArrayList<>();
+
+            // TODO consultar informacion de clientes
+            Client client = new Client();
+            client.setId(row.getClientId());
+
+            Optional<List<Detail>> details = this.repository.findAllDetail(row.getId());
+
+            LOG.debug("SEQUENCE: {} | PRODUCTS: {}", row.getSequence(), details.get().size());
+
+            for (Detail rowd : details.get()) {
+                // TODO hacer llamado queue informacion de producto
+                Product product = new Product();
+                product.setCode(rowd.getProductCode());
+
+                co.com.demo.bills.domain.controller.Detail item = new co.com.demo.bills.domain.controller.Detail();
+                item.setId(rowd.getId());
+                item.setBillId(rowd.getBillId());
+                item.setProduct(product);
+                item.setDiscount(rowd.getDiscount());
+                item.setQuantity(rowd.getQuantity());
+
+                detailList.add(item);
+            }
+
+            co.com.demo.bills.domain.controller.Bill item = new co.com.demo.bills.domain.controller.Bill();
+            item.setId(row.getId());
+            item.setSequence(row.getSequence());
+            item.setDate(row.getDate());
+            item.setClient(client);
+            item.setDetail(detailList);
+
+            billList.add(item);
+        }
+
+        return Optional.of(billList);
     }
 }
--- src/main/java/co/com/demo/bills/infraestructure/controller/BillController.java
+++ src/main/java/co/com/demo/bills/infraestructure/controller/BillController.java
@@ -1,22 +1,39 @@
 package co.com.demo.bills.infraestructure.controller;
 
+import co.com.demo.bills.application.BillBehavior;
+import co.com.demo.bills.domain.controller.Bill;
+import co.com.demo.bills.exceptions.bussines.BillNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class BillController {
 
-    @PostMapping("/bills")
-    public ResponseEntity<String> detail(@RequestBody(required = true) String data) {
-        return null;
+    private final BillBehavior services;
+
+    @GetMapping("/bills")
+    public ResponseEntity<List<Bill>> detail() {
+        Optional<List<Bill>> bills = this.services.getAllBills();
+
+        if (bills.isEmpty()) throw new BillNotFoundException("There are not bills informations!");
+
+        return new ResponseEntity<>(bills.get(), HttpStatus.OK);
     }
 
-    @GetMapping("/bills/{number}")
-    public ResponseEntity<String> bills(@RequestBody(required = true) String number) {
-        return null;
+    @GetMapping("/bills/{sequence}")
+    public ResponseEntity<Bill> bills(@PathVariable(required = true) String sequence) {
+        Optional<Bill> bills = this.services.getBillById(sequence);
+
+        if (!bills.isPresent()) throw new BillNotFoundException(String.format("There is no information for bill with sequence: [%s]", sequence));
+
+        return new ResponseEntity<>(bills.get(), HttpStatus.OK);
     }
 
 }
